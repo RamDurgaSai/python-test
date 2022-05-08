2e1873b88b61020ed71a5fa7e7e8963f52cd03c3
Ram Durga Sai <57700161+RamDurgaSai@users.noreply.github.com>
2022-05-08 20:19:06 +0530

    Updated

--- main.py
+++ main.py
@@ -1,14 +1,63 @@
-import pymongo
+'''
+Schema:-
 
 
-def main():
+BookMaker => {
+    "id" : integer,
+    "name" : string,
+    "isActive" : boolean
+}
+
+
+Event => {
+    "id" : integer,
+    "name" : string,
+    "isActive" : boolean,
+    .   .       .       .
+    .   .       .       .
+    .   .       .       .
+    "__"  : "---"
+}
+
+
+Pros:- 
+    1. instead of regular loops, used list compression 
+    2. instead of inserting one by one, used the powerful method insert many 
+    3. In this case, we are transferring event details to multiple bookmakers so that the bookmaker can easily know event details from the transferred data. 
+
+Cons:- 
+    1. Time complexity is n*m ( where m is number of events and n is number of bookmaker)
+    2. Iterations makes code runtime slower
     
-    mongoClient = pymongo.MongoClient("mongodb://localhost:27017", username="admin", password="admin")
 
-    collection1 = mongoClient.db1.activeEvents
-    map = mongoClient.oddsandmore.mapping
-
-    for d in collection1.find({"bookmakerId" : 12 , "bookmakerName" : "goldbet" }) : map.insert_one(d)
+'''
 
 
-main()
+# Implementation ...!
+from pymongo import MongoClient  # let's import mongoclient
+
+client = MongoClient("https://admin:admin_password@localhost:27017") # MongoDB client 
+
+events, bookmakers = client.events.events, client.bookmakers.bookmaker # let's get data collectoins !
+
+
+client.oddsandmore.maps.insert_many( # insert mapped data into database .. !
+    [
+        event.update(
+            {
+                "bookmakerId"  : bookmaker.id,
+                "bookmakerName" : bookmaker.name
+            } # let's update event with bookmaker detials 
+        )
+        for event in events.find("{}") # lets fetch all events ... 
+        for bookmaker in bookmakers.find({'isActive' : True}) # lets fetch bookmakers and filter it 
+        
+    ]
+    
+)
+
+
+
+
+
+
